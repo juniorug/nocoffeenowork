984c3ce8b08eb1461a808a6f813f8e9dc588689e
Junior Mascarenhas <juniorug@gmail.com>
2018-07-04 23:43:16 -0300

    fixing bugs

--- atualizaLista.php
+++ atualizaLista.php
@@ -1,4 +1,5 @@
 <?
+
 	$purchase_id = 0;
     $count_produtos_ativos = 0;	
     $siteUrl = "http://nocoffeenowork.juniormascarenhas.com/";
@@ -6,7 +7,7 @@
 	$username = "u765057031_user";
 	$password = "Eisa2015";
 	$dbname = "u765057031_bd";
-
+	$msg='';
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
@@ -14,41 +15,48 @@
 		die("Connection failed: " . $conn->connect_error);
 	} 
 	$purchase_id = $conn->query("SELECT id FROM pedido WHERE pedido_atual = 1;")->fetch_object()->id;
+	echo '$purchase_id: '.$purchase_id."<br>";
 	$count_produtos_ativos = $conn->query("SELECT COUNT(1) AS countativos FROM  produto WHERE ativo =1;")->fetch_object()->countativos;
 
     $person_id = isset($_POST['selname']) ? $_POST['selname'] : false;
+    echo '$person_id: '.$person_id."<br>";
+    echo '$count_produtos_ativos: '.$count_produtos_ativos."<br>";
     if ($person_id) {
 	  for ($product_id = 1; $product_id <= $count_produtos_ativos; $product_id++) {
 		$selname = 'sel'.$product_id;
 		$qtde_produto = isset($_POST[$selname]) ? $_POST[$selname] : false;
+		echo '$product_id: '.$product_id."<br>";
+		echo '$qtde_produto: '.$qtde_produto."<br>";
 		if ($qtde_produto) {
 			$call_proc =  'CALL insert_PPP('.$person_id.','.$product_id.','.$purchase_id.','.$qtde_produto.')';				
 			if (!$conn->query($call_proc)) {
-				echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-				$msg = 'Houve um erro ao tentar alterar o pedido, por favor entre em contato com o administrador.';
+			    echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+				$msg = 'Houve um erro ao tentar alterar o pedido, por favor entre em contato com o administrador. ERR02';
 				$ok = 2;
 			} else {
+			    echo 'FOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII';
 				$msg = 'Obrigado! Seu pedido foi adicionado ao inventário. Nós entraremos em contato com você em breve.';
 				$ok = 1; 
 			}
-		} else {
+		} /*else {
 			//criar procedure que procura se há pedido para esse usuario e produto, faz update para qtde=0,ativo=0,val_total=0
 			$call_proc =  'CALL deactivate_PPP('.$person_id.','.$product_id.','.$purchase_id.')';				
 			if (!$conn->query($call_proc)) {
-				echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-				$msg = 'Houve um erro ao tentar alterar o pedido, por favor entre em contato com o administrador.';
-				$ok = 2;
+			    echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+				$msg = 'Houve um erro ao tentar alterar o pedido, por favor entre em contato com o administrador. ERR03';
+				$ok = 3;
 			} else {
 				$msg = 'Obrigado! Seu pedido foi adicionado ao inventário. Nós entraremos em contato com você em breve.';
 				$ok = 1; 
 			}
-		}
+		}*/
 	  }
 	  $conn->close();		
 	} else {
-	   $msg = "Erro! Selecione o nome do usuário";
+	   $msg = "Erro! Selecione o nome do usuário.";
 	   $ok = 2; 	
    }
+   //echo '$msg:'.$msg."<br>";
    header('Location: '.$siteUrl.'?Ok='.urlencode($ok).'&msg='.urlencode($msg)); exit;		
 
 ?>
